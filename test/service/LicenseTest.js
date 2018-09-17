10f34680876798dba047883cab8fa9964dbdb6d8
Chris Deighton <chris@serato.com>
2018-09-17 23:45:57 +1200

    Add a test that unwraps an error response

---
+++ test/service/LicenseTest.js
@@ -0,0 +1,30 @@
+import { SwsClient, serviceUriDefault } from '../../src/index'
+import { describe, it } from 'mocha'
+import assert from 'assert'
+import nock from 'nock'
+import { expect } from 'chai'
+
+const appId = 'myClientAppId'
+
+describe('License', function () {
+  it('throws Forbidden error with `Access denied` response', function () {
+    nock('https://' + serviceUriDefault.license)
+      .get('/api/v1/me/licenses', '')
+      .reply(403, {
+        'code': 2000,
+        'error': 'Access denied. Invalid grants.'
+      })
+
+    let client = new SwsClient({ appId: appId })
+
+    return client.license.getLicenses().then(
+      () => Promise.reject(new Error('Expected non-2xx HTTP response code')),
+      err => {
+        expect(err.httpStatus).to.equal(403)
+        return err.response.json()
+      }
+    ).then(
+      data => assert.strictEqual(data.code, 2000)
+    )
+  })
+})
