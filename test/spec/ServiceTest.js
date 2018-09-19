36e34bd410ede1c0c8ca63dffddad56af6bc4133
Chris Deighton <chris@serato.com>
2018-09-19 23:46:48 +1200

    Add test coverage for all custom handlers

--- test/spec/ServiceTest.js
+++ test/spec/ServiceTest.js
@@ -15,9 +15,12 @@ import { expect } from 'chai'
 const appId = 'myClientAppId'
 const licenseServiceHost = 'https://' + serviceUriDefault.license
 const getLicensesUri = '/api/v1/me/licenses'
-const customAccessDeniedHandlerResponse = 'This value is returned by our custom accessDeniedHandler'
+const customHandlerResponse = 'This value is returned by our custom handler'
 
 describe('Service', function () {
+  /**
+   * Test the happy path
+   */
   it('returns message body when `getLicenses` executed successfully', function () {
     let body = {
       'some': 'body content',
@@ -33,44 +36,116 @@ describe('Service', function () {
     )
   })
 
-  it('handles `403 Forbidden` + error code `2000` with default Promise reject when no custom handler is supplied', function () {
-    nock(licenseServiceHost).get(getLicensesUri, '').reply(
-      403, { 'code': 2000, 'error': 'Access denied. Invalid grants.' }
-    )
+  /**
+   * Test the custom error handlers.
+   *
+   * Iterate over a data set the specifies the the combination of HTTP status
+   * and error code that, if received, will trigger a custom error handler (if specified).
+   *
+   * Two tests are executed for each data item:
+   *
+   * One the doesn't specify a customer handler and asserts that the default error handler is used.
+   * One the sets a custom error handler and asserts that it is used.
+   */
 
-    let client = new Sws({ appId: appId })
+  // Define the custom error handler. The err handler receives the error object returned
+  // from the HTTP request.
+  let customErrorHandler = (err) => {
+    return `${customHandlerResponse} ${err.response.status} - ${err.response.data.code}`
+  }
 
-    return client.license.getLicenses().then(
-      () => Promise.reject(new Error('Expected non-2xx HTTP response code')),
-      err => {
-        // Error codes should be exposed via Error object...
-        expect(err.httpStatus).to.equal(403)
-        expect(err.code).to.equal(2000)
-        // But check that we have the underlying axios response object
-        // is in the Error too
-        expect(err.response.status).to.equal(403)
-      }
-    )
-  })
-
-  it('handles `403 Forbidden` + error code `2000` with custom handler when supplied', function () {
-    nock(licenseServiceHost).get(getLicensesUri, '').reply(
-      403, { 'code': 2000, 'error': 'Access denied. Invalid grants.' }
-    )
-
-    let client = new Sws({ appId: appId })
-
-    client.license.accessDeniedHandler = (err) => {
-      return customAccessDeniedHandlerResponse + ' ' + err.response.status + ' - ' + err.response.data.code
+  let customHandlerTests = [
+    // Access token is invalid (eg. bad signature)
+    {
+      httpStatus: 403,
+      statusText: 'Forbidden',
+      code: 2001,
+      error: 'Invalid Aaccess token',
+      handlerName: 'invalidAccessTokenHandler',
+      attachHandler: (client) => { client.invalidAccessTokenHandler = customErrorHandler }
+    },
+    // Access token has expired
+    {
+      httpStatus: 401,
+      statusText: 'Unauthorized',
+      code: 2002,
+      error: 'Expired Access token',
+      handlerName: 'invalidAccessTokenHandler',
+      attachHandler: (client) => { client.invalidAccessTokenHandler = customErrorHandler }
+    },
+    // Access token does not contain required scopes
+    {
+      httpStatus: 403,
+      statusText: 'Forbidden',
+      code: 2000,
+      error: 'Access denied. Invalid grants.',
+      handlerName: 'accessDeniedHandler',
+      attachHandler: (client) => { client.accessDeniedHandler = customErrorHandler }
+    },
+    // Refresh token is invalid (eg. bad signature)
+    {
+      httpStatus: 400,
+      statusText: 'Bad Request',
+      code: 1001,
+      error: 'Invalid Refresh token.',
+      handlerName: 'invalidRefreshTokenHandler',
+      attachHandler: (client) => { client.invalidRefreshTokenHandler = customErrorHandler }
+    },
+    // Refresh token has expired
+    {
+      httpStatus: 400,
+      statusText: 'Bad Request',
+      code: 1007,
+      error: 'Expired Refresh token.',
+      handlerName: 'invalidRefreshTokenHandler',
+      attachHandler: (client) => { client.invalidRefreshTokenHandler = customErrorHandler }
     }
+  ]
 
-    return client.license.getLicenses().then(
-      data => expect(data).to.equal(customAccessDeniedHandlerResponse + ' 403 - 2000'),
-      err => {
-        let error = new Error('Expected error to be handled by custom `accessDeniedHandler` handler')
-        error.error = err
-        Promise.reject(error)
-      }
-    )
+  customHandlerTests.forEach(test => {
+    it(`handles '${test.httpStatus} ${test.statusText}, error code ${test.code}' with default error handler`, function () {
+      // Intercept the HTTP request and return the desired HTTP status and JSON message body
+      nock(licenseServiceHost).get(getLicensesUri, '').reply(
+        test.httpStatus,
+        { 'code': test.code, 'error': test.error }
+      )
+
+      let client = new Sws({ appId: appId })
+
+      return client.license.getLicenses().then(
+        // Should never hit the `resolve` callback
+        () => Promise.reject(new Error('Expected non-2xx HTTP response code')),
+        // Should always hit the `reject` callback
+        err => {
+          expect(err.httpStatus).to.equal(test.httpStatus)
+          expect(err.code).to.equal(test.code)
+          expect(err.response.status).to.equal(test.httpStatus)
+        }
+      )
+    })
+
+    it(`handles '${test.httpStatus} ${test.statusText}, error code ${test.code}' with custom handler`, function () {
+      // Intercept the HTTP request and return the desired HTTP status and JSON message body
+      nock(licenseServiceHost).get(getLicensesUri, '').reply(
+        test.httpStatus,
+        { 'code': test.code, 'error': test.error }
+      )
+
+      let client = new Sws({ appId: appId })
+
+      // Attach the customer handler to the client
+      test.attachHandler(client.license)
+
+      return client.license.getLicenses().then(
+        // Should always hit the `resolve` callback because we're using our custom handler
+        data => expect(data).to.equal(`${customHandlerResponse} ${test.httpStatus} - ${test.code}`),
+        // Should never hit the `reject` callback
+        err => {
+          let error = new Error(`Expected error to be handled by custom ${test.handlerName} handler`)
+          error.error = err
+          Promise.reject(error)
+        }
+      )
+    })
   })
 })
