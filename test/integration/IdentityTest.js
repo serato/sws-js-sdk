5c05d0fce014a82d3aaf5e86d1a5724982e80c6b
harrietrc <harrietrc@gmail.com>
2018-10-02 14:41:06 +1300

    [WEB-2282] Move URI validation tests to new test scripts

--- test/integration/IdentityTest.js
+++ test/integration/IdentityTest.js
@@ -1,4 +1,4 @@
-import { SwsClient } from '../../src/index'
+import Sws, { SwsClient } from '../../src/index'
 import { describe, it, before } from 'mocha'
 import { expect } from 'chai'
 import environment from '../../environment.json'
@@ -14,6 +14,41 @@ const {
 const timeout = 5000
 
 describe('Identity', function () {
+  describe('Identity URI Validation Tests', function () {
+    it(`confirms URI used in 'tokenRefresh()' method, by returning a non-404 HTTP response`, function () {
+      let sws = new Sws({ appId: appId })
+
+      return sws.id.tokenRefresh('token.value').then(
+        () => Promise.reject(new Error('Expected non-2xx HTTP response code')),
+        err => {
+          expect(err.httpStatus).not.to.equal(404)
+        }
+      )
+    })
+
+    it(`confirms URI used in 'getMe()' method, by returning a non-404 HTTP response`, function () {
+      let sws = new Sws({ appId: appId })
+
+      return sws.id.getUserMe().then(
+        () => Promise.reject(new Error('Expected non-2xx HTTP response code')),
+        err => {
+          expect(err.httpStatus).not.to.equal(404)
+        }
+      )
+    })
+
+    it(`confirms URI used in 'login()' method, by returning a non-404 HTTP response`, function () {
+      let sws = new Sws({ appId: appId })
+
+      return sws.id.login().then(
+        () => Promise.reject(new Error('Expected non-2xx HTTP response code')),
+        err => {
+          expect(err.httpStatus).not.to.equal(404)
+        }
+      )
+    })
+  })
+
   describe('makes valid requests to the /me endpoint', function () {
     let sws // Initialised in the before hook
 
--- test/integration/LicenseTest.js
+++ test/integration/LicenseTest.js
@@ -36,6 +36,50 @@ describe('License Tests', function () {
         }
       )
     })
+
+    it(`confirms URI used in 'getProductType()' method, by returning a non-404 HTTP response`, function () {
+      let sws = new Sws({ appId: appId })
+
+      return sws.license.getProductType(0).then(
+        () => Promise.reject(new Error('Expected non-2xx HTTP response code')),
+        err => {
+          expect(err.httpStatus).not.to.equal(404)
+        }
+      )
+    })
+
+    it(`confirms URI used in 'getProductTypes()' method, by returning a non-404 HTTP response`, function () {
+      let sws = new Sws({ appId: appId })
+
+      return sws.license.getProductTypes().then(
+        () => Promise.reject(new Error('Expected non-2xx HTTP response code')),
+        err => {
+          expect(err.httpStatus).not.to.equal(404)
+        }
+      )
+    })
+
+    it(`confirms URI used in 'getProducts()' method with no user ID, by returning a non-404 HTTP response`, function () {
+      let sws = new Sws({ appId: appId })
+
+      return sws.license.getProducts().then(
+        () => Promise.reject(new Error('Expected non-2xx HTTP response code')),
+        err => {
+          expect(err.httpStatus).not.to.equal(404)
+        }
+      )
+    })
+
+    it(`confirms URI used in 'getProducts()' method with user ID, by returning a non-404 HTTP response`, function () {
+      let sws = new Sws({ appId: appId })
+
+      return sws.license.getProducts({ userId: 123 }).then(
+        () => Promise.reject(new Error('Expected non-2xx HTTP response code')),
+        err => {
+          expect(err.httpStatus).not.to.equal(404)
+        }
+      )
+    })
   })
 
   describe('makes valid requests to the /products/types endpoint', function () {
