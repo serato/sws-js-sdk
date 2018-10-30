2b310858b7f691fd898f4bf9d705d9dc1c60e366
harrietrc <harrietrc@gmail.com>
2018-10-31 09:47:47 +1300

    [WEB-2298] Add Delete Payment Method endpoints

--- src/Ecom.js
+++ src/Ecom.js
@@ -34,16 +34,33 @@ export default class Ecom extends Service {
   }
 
   /**
-  * Return payment methods added by a logged-in user.
-  * Requires a valid access token.
-  *
-  * @returns {Promise}
-  */
+   * Return payment methods added by a logged-in user.
+   * Requires a valid access token.
+   *
+   * @returns {Promise}
+   */
   getPaymentMethods () {
     return this.fetch(
       this.bearerTokenAuthHeader(),
       this.userId === 0 ? '/api/v1/me/paymentmethods' : '/api/v1/users/' + this.userId + '/paymentmethods',
       null
+    )
+  }
+
+  /**
+   * Delete a payment method identified by a given payment token. The payment method's customerId must match the user's
+   * ID.
+   * Requires a valid access token.
+   *
+   * @param paymentToken Token identifying the payment method on Braintree
+   * @return {Promise}
+   */
+  deletePaymentMethod (paymentToken) {
+    return this.fetch(
+      this.bearerTokenAuthHeader(),
+      this.userId === 0 ? `/api/v1/me/paymentmethods/${paymentToken}` : `/api/v1/users/${this.userId}/paymentmethods/${paymentToken}`,
+      null,
+      'DELETE'
     )
   }
 }
