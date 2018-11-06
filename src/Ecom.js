7adae9203b3a6a0e0ca84a58ad620841653c7ea6
harrietrc <harrietrc@gmail.com>
2018-11-07 10:46:29 +1300

    [WEB-2279] Add updateSubscription method

--- src/Ecom.js
+++ src/Ecom.js
@@ -46,4 +46,25 @@ export default class Ecom extends Service {
       null
     )
   }
+
+  /**
+   * Update the payment method and number of billing cycles of a subscription identified by a given subscription ID.
+   * Requires a valid access token.
+   *
+   * @param {Object} param Options
+   * @param {String} param.subscriptionId ID of the subscription to update
+   * @param {String} param.paymentToken
+   * @param {Number} param.numberOfBillingCycle
+   * @return {Promise}
+   */
+  updateSubscription ({ subscriptionId, paymentToken, numberOfBillingCycle }) {
+    return this.fetch(
+      this.bearerTokenAuthHeader(),
+      this.userId === 0 ? '/api/v1/me/subscriptions/' + subscriptionId : '/api/v1/users/' + this.userId + '/subscriptions/' + subscriptionId,
+      this.toBody({
+        'number_of_billing_cycle': numberOfBillingCycle, 'payment_method_token': paymentToken
+      }),
+      'PUT'
+    )
+  }
 }
