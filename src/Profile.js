5429f3db27f645948da21cfcb9a94d7884eacaa9
ben-farrelly <benjamin.farrelly@serato.com>
2019-01-24 10:29:52 +1300

    [WEB-2394] Support for forum notifications

--- src/Profile.js
+++ src/Profile.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import Service from'./Service'
+import Service from './Service'
 /**
  * Profile Service class
  *
@@ -23,66 +23,75 @@ export default class Profile extends Service {
    *
    * @returns {Promise}
    */
-    getProfile () {
-      return this.fetch(
-        this.bearerTokenAuthHeader(),
-        this.userId === 0 ? '/api/v1/me' : '/api/v1/users/' + this.userId,
-        null,
-        'GET'
-      )
-    }
-    /**
-     * Update a user's profile.
-     * Requires a valid access token
-     * @param   {Object} param
-     * @param   {Number} param.globalContactStatus
-     * @param   {String} param.emailAddress
-     * @param   {String} param.firstName
-     * @param   {String} param.lastName
-     * @param   {String} param.djName
-     * @param   {String} param.locale
-     * @param   {String} param.address1
-     * @param   {String} param.address2
-     * @param   {String} param.city
-     * @param   {String} param.region
-     * @param   {Number} param.postCode
-     * @param   {String} param.country
-     * @returns {Promise}
-     */
-    updateProfile ({
-        globalContactStatus,
-        emailAddress,
-        firstName,
-        lastName,
-        djName,
-        locale,
-        address1,
-        address2,
-        city,
-        region,
-        postCode,
-        country }
-        = {}
-    ) {
-      return this.fetch(
-        this.bearerTokenAuthHeader(),
-        this.userId === 0 ? '/api/v1/me' : '/api/v1/users/' + this.userId,
-        this.toBody({
-          global_contact_status: globalContactStatus,
-          email_address: emailAddress,
-          first_name: firstName,
-          last_name: lastName,
-          dj_name: djName,
-          locale: locale,
-          address_1: address1,
-          address_2: address2,
-          city: city,
-          region: region,
-          post_code: postCode,
-          country: country
-        }),
-        'PUT'
-      )
-    }
-  
+  getProfile () {
+    return this.fetch(
+      this.bearerTokenAuthHeader(),
+      this.userId === 0 ? '/api/v1/me' : '/api/v1/users/' + this.userId,
+      null,
+      'GET'
+    )
+  }
+  /**
+   * Update a user's profile.
+   * Requires a valid access token
+   * @param   {Object} param
+   * @param   {Number} param.globalContactStatus
+   * @param   {String} param.firstName
+   * @param   {String} param.lastName
+   * @param   {String} param.djName
+   * @param   {String} param.locale
+   * @param   {String} param.address1
+   * @param   {String} param.address2
+   * @param   {String} param.city
+   * @param   {String} param.region
+   * @param   {Number} param.postCode
+   * @param   {String} param.country
+   * @param   {Boolean} param.notifyTracked
+   * @param   {Boolean} param.notifyPrivate
+   * @param   {Boolean} param.autoRead
+   * @param   {Boolean} param.autoSubscribe
+   * @returns {Promise}
+   */
+  updateProfile ({
+    globalContactStatus,
+    firstName,
+    lastName,
+    djName,
+    locale,
+    address1,
+    address2,
+    city,
+    region,
+    postCode,
+    country,
+    notifyTracked,
+    notifyPrivate,
+    autoRead,
+    autoSubscribe
+  }
+  = {}
+  ) {
+    return this.fetch(
+      this.bearerTokenAuthHeader(),
+      this.userId === 0 ? '/api/v1/me' : '/api/v1/users/' + this.userId,
+      this.toBody({
+        global_contact_status: globalContactStatus,
+        first_name: firstName,
+        last_name: lastName,
+        dj_name: djName,
+        locale: locale,
+        address_1: address1,
+        address_2: address2,
+        city: city,
+        region: region,
+        post_code: postCode,
+        country: country,
+        notify_tracked: notifyTracked,
+        notify_private: notifyPrivate,
+        auto_read: autoRead,
+        auto_subscribe: autoSubscribe
+      }),
+      'PUT'
+    )
+  }
 }
