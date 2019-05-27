f4fd66d14603ffdec89833236acf14a4a9ffdd73
Chris Deighton <chris@serato.com>
2019-05-27 22:09:53 +1200

    Adds endpoints for avatar editing in Profile service
    * Adds `Profile.updateAvatar` and `Profile.deleteAvatar` methods
    * Bumps version to 1.5.0

--- test/integration/ProfileTest.js
+++ test/integration/ProfileTest.js
@@ -106,4 +106,56 @@ describe('Profile Tests', function () {
       )
     })
   })
+
+  it(`confirms URI in 'updateAvatar()' method without user ID, by returning a non-404 response`, function () {
+    swsClient.userId = 0
+    return swsClient.profile.updateAvatar({
+      uploadType: 'avatar',
+      contentType: 'image/jpeg'
+    }).then(
+      () => Promise.reject(new Error('Expected non-2xx HTTP response code')),
+      err => {
+        expect(err.httpStatus).not.to.equal(404)
+      }
+    )
+  })
+
+  it(`confirms URI in 'updateAvatar()' method with a user ID, by returning a non-404 response`, function () {
+    swsClient.userId = 123
+    return swsClient.profile.updateAvatar({
+      uploadType: 'avatar',
+      contentType: 'image/jpeg'
+    }).then(
+      () => Promise.reject(new Error('Expected non-2xx HTTP response code')),
+      err => {
+        expect(err.httpStatus).not.to.equal(404)
+      }
+    )
+  })
+
+  it(`confirms URI in 'deleteAvatar()' method without user ID, by returning a non-404 response`, function () {
+    swsClient.userId = 0
+    return swsClient.profile.deleteAvatar({
+      uploadType: 'avatar',
+      contentType: 'image/jpeg'
+    }).then(
+      () => Promise.reject(new Error('Expected non-2xx HTTP response code')),
+      err => {
+        expect(err.httpStatus).not.to.equal(404)
+      }
+    )
+  })
+
+  it(`confirms URI in 'deleteAvatar()' method with a user ID, by returning a non-404 response`, function () {
+    swsClient.userId = 123
+    return swsClient.profile.deleteAvatar({
+      uploadType: 'avatar',
+      contentType: 'image/jpeg'
+    }).then(
+      () => Promise.reject(new Error('Expected non-2xx HTTP response code')),
+      err => {
+        expect(err.httpStatus).not.to.equal(404)
+      }
+    )
+  })
 })
