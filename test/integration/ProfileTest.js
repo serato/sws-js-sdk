6c50f00d3296b1df9335c10c4eeef6a2b6ce420e
Guillaume Brousse <guillaume.brousse@serato.com>
2019-08-12 11:09:26 +1200

    Add test for validateAll endpoint to ProfileTest.js

--- test/integration/ProfileTest.js
+++ test/integration/ProfileTest.js
@@ -228,6 +228,26 @@ describe('Profile Tests', function () {
         }
       )
     })
+
+    it(`confirms URI in 'validateAllBetaProgram()' method without user ID, by returning a non-404 response`, function () {
+      swsClient.userId = 0
+      return swsClient.profile.validateAllBetaProgram().then(
+        () => Promise.reject(new Error('Expected non-2xx HTTP response code')),
+        err => {
+          expect(err.httpStatus).not.to.equal(404)
+        }
+      )
+    })
+
+    it(`confirms URI in 'validateAllBetaProgram()' method with a user ID, by returning a non-404 response`, function () {
+      swsClient.userId = 123
+      return swsClient.profile.validateAllBetaProgram().then(
+        () => Promise.reject(new Error('Expected non-2xx HTTP response code')),
+        err => {
+          expect(err.httpStatus).not.to.equal(404)
+        }
+      )
+    })
   })
 
 })
