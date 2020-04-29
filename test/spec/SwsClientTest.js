4d270c735f2f935c683b05ee20e1ac8d2e6dd265
Chris Deighton <chris@serato.com>
2020-04-30 00:24:04 +1200

    Updates unit tests

--- test/spec/SwsClientTest.js
+++ test/spec/SwsClientTest.js
@@ -37,11 +37,11 @@ describe('SwsClient', function () {
           .reply(200, {
             'access': {
               'token': newAccessTokenValue,
-              'expires_at': accessTokenExpiresAt.valueOf()
+              'expires_at': (accessTokenExpiresAt.valueOf() / 1000)
             },
             'refresh': {
               'token': newRefreshTokenValue,
-              'expires_at': refreshTokenExpiresAt
+              'expires_at': (refreshTokenExpiresAt.valueOf() / 1000)
             }
           })
           .get(getLicensesUri, '')
@@ -73,11 +73,11 @@ describe('SwsClient', function () {
           .reply(200, {
             'access': {
               'token': newAccessTokenValue,
-              'expires_at': accessTokenExpiresAt.valueOf()
+              'expires_at': (accessTokenExpiresAt.valueOf() / 1000)
             },
             'refresh': {
               'token': newRefreshTokenValue,
-              'expires_at': refreshTokenExpiresAt
+              'expires_at': (refreshTokenExpiresAt.valueOf() / 1000)
             }
           })
           .get(getLicensesUri, '')
@@ -110,11 +110,11 @@ describe('SwsClient', function () {
           .reply(200, {
             'access': {
               'token': newAccessTokenValue,
-              'expires_at': accessTokenExpiresAt.valueOf()
+              'expires_at': (accessTokenExpiresAt.valueOf() / 1000)
             },
             'refresh': {
               'token': newRefreshTokenValue,
-              'expires_at': refreshTokenExpiresAt
+              'expires_at': (refreshTokenExpiresAt.valueOf() / 1000)
             }
           })
           .get(getLicensesUri, '')
@@ -150,11 +150,11 @@ describe('SwsClient', function () {
           .reply(200, {
             'access': {
               'token': newAccessTokenValue,
-              'expires_at': accessTokenExpiresAt.valueOf()
+              'expires_at': (accessTokenExpiresAt.valueOf() / 1000)
             },
             'refresh': {
               'token': newRefreshTokenValue,
-              'expires_at': refreshTokenExpiresAt
+              'expires_at': (refreshTokenExpiresAt.valueOf() / 1000)
             }
           })
           .get(getLicensesUri, '')
@@ -296,9 +296,10 @@ describe('SwsClient', function () {
   describe('successfully updates expired access token', function () {
     it(`Calls 'accessTokenUpdatedHandler' handler after successfully updating access token`, function () {
       // FYI, JS timestamps use milliseconds but our web service returns timestamps in seconds
-      // the SwsClient under test has code that coverts a server timestamp into a JavaScript date.
+      // The SwsClient under test has code that coverts a server timestamp into a JavaScript date.
       // So this test includes some maths that takes that into account
       let accessTokenExpiresAt = new Date(Date.now() + 3600000) // Add 1 hour (in milliseconds)
+      let refreshTokenExpiresAt = new Date(Date.now() + (24 * 3600000))
 
       let scope = nock(/serato/)
         .get(getLicensesUri, '')
@@ -308,14 +309,20 @@ describe('SwsClient', function () {
           'access': {
             'token': newAccessTokenValue,
             'expires_at': (accessTokenExpiresAt.valueOf() / 1000) // Conver to second-based unix timestamp
+          },
+          'refresh': {
+            'token': newRefreshTokenValue,
+            'expires_at': (refreshTokenExpiresAt.valueOf() / 1000)
           }
         })
         .get(getLicensesUri, '')
         .reply(200, { 'some': 'response' })
 
-      let accessTokenUpdatedHandler = (token, expires) => {
-        expect(token).to.equal(newAccessTokenValue)
-        expect(expires).to.eql(accessTokenExpiresAt)
+      let accessTokenUpdatedHandler = (accessToken, accessTokenExpires, refreshToken, refreshTokenExpires) => {
+        expect(accessToken).to.equal(newAccessTokenValue)
+        expect(accessTokenExpires.valueOf()).to.eql(accessTokenExpiresAt.valueOf())
+        expect(refreshToken).to.equal(newRefreshTokenValue)
+        expect(refreshTokenExpires.valueOf()).to.equal(refreshTokenExpiresAt.valueOf())
       }
 
       let sws = new SwsClient({ appId: appId })
