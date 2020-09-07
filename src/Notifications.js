a248cca829a900b4dd191c51e6f7d3bbed363fd8
Takayuki Ibaraki <takayuki.ibaraki@serato.com>
2020-09-07 17:09:42 +1200

    Clean up `getNotifications` implementation and documentation

--- src/Notifications.js
+++ src/Notifications.js
@@ -21,15 +21,17 @@ export default class Notifications extends Service {
 
   /**
    * Retrieve a list of notifcations to a client application.
-   * @param  {String} hostAppName
-   * @param  {String} hostAppVersion
-   * @param  {String} hostAppOs
-   * @param  {String} locale
+   *
+   * @param  {Object} params - Input parameter object
+   * @param  {?String} params.hostAppName - Host app name to filter the results by
+   * @param  {?String} params.hostAppVersion - Host app version to filter by
+   * @param  {?String} params.hostAppOs - Host app operating system to filter by
+   * @param  {?String} params.locale - Locale of the client to indicate content language preference
    * @return {Promise}
    */
   getNotifications ({ hostAppName = null, hostAppVersion = null, hostAppOs = null, locale = null }) {
     return this.fetch(
-      this.bearerTokenAuthHeader() === null ? null : this.bearerTokenAuthHeader(),
+      this.bearerTokenAuthHeader(),
       '/api/v1/notifications',
       this.toBody({
         'host_app_name': hostAppName,
