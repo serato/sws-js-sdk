3a04fcbf216b6e6bb4e7fd372ed53015372d1c65
Takayuki Ibaraki <takayuki.ibaraki@serato.com>
2020-09-08 11:59:28 +1200

    Update optional parameter docs

--- src/Notifications.js
+++ src/Notifications.js
@@ -23,10 +23,10 @@ export default class Notifications extends Service {
    * Retrieve a list of notifcations to a client application.
    *
    * @param  {Object} params - Input parameter object
-   * @param  {?String} params.hostAppName - Host app name to filter the results by
-   * @param  {?String} params.hostAppVersion - Host app version to filter by
-   * @param  {?String} params.hostAppOs - Host app operating system to filter by
-   * @param  {?String} params.locale - Locale of the client to indicate content language preference
+   * @param  {?String} [params.hostAppName = null]    - Host app name to filter the results by
+   * @param  {?String} [params.hostAppVersion = null] - Host app version to filter by
+   * @param  {?String} [params.hostAppOs = null]      - Host app operating system to filter by
+   * @param  {?String} [params.locale = null]         - Locale of the client to indicate content language preference
    * @return {Promise}
    */
   getNotifications ({ hostAppName = null, hostAppVersion = null, hostAppOs = null, locale = null }) {
@@ -48,8 +48,8 @@ export default class Notifications extends Service {
    * Requires a valid access token.
    *
    * @param {Object} params - Input parameter object
-   * @param {?String} params.status - Status of the campaign to filter by. Must be one of 'active', 'draft' or
-   *                                  'archived' if set
+   * @param {?String} [params.status = null] - Status of the campaign to filter by. Must be one of 'active', 'draft' or
+   *                                           'archived' if set
    * @return {Promise}
    */
   getCampaigns ({ status = null }) {
