d51de595b9555b8348ab4421f69d60e82835525e
sameeraroshan-serato <sameera.roshan@serato.com>
2024-08-06 09:11:57 +1200

    [WEB-10273] Added DownloadHistory return type.

--- src/DigitalAssets.js
+++ src/DigitalAssets.js
@@ -54,6 +54,21 @@ import Service from './Service'
  * @property {String} url Download URL
  * @property {String} url_created Creation time of the download URL expressed in ISO ISO 8061 date format.
  * @property {String} url_expires Expiry time of the download URL expressed in ISO ISO 8061 date format.
+ *
+ * @typedef {Object} AssetWithoutResources
+ * @property {Number} id
+ * @property {String} name
+ * @property {'application_installer' | 'content_pack'} type
+ * @property {ReleaseType} release_type
+ * @property {String} version
+ * @property {HostApplication[]} host_app_compatibility A list of compatible host applications
+ * @property {String} release_date Release date in ISO 8061 format
+ * @property {String} [webpage_url = undefined] Canonical URL for a webpage associated with the asset
+ * @property {Object<string, any>} [meta = undefined] Meta data associated with the asset
+ *
+ * @typedef {Object} DownloadHistory
+ * @property {AssetWithoutResources} asset
+ * @property {Resource} resource
  */
 
 /**
@@ -158,7 +173,7 @@ export default class DigitalAssetsService extends Service {
    * Get user application installer download history
    * @param  {Object} [param = undefined] param
    * @param  {HostApplicationName} [param.hostAppName = undefined] param.hostAppName
-   * @return {Promise<AssetDownload>}
+   * @return {Promise<DownloadHistory>}
    */
   getApplicationInstallerDownloads ({ hostAppName }) {
     return this.fetch(
