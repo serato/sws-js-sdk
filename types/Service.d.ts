7ac5370b236e7e709edcfb1d2c6c18ccbfef6de5
HayleySharpeSerato <hayley.sharpe@serato.com>
2024-12-05 13:27:49 +1300

    create SwsError class

--- types/Service.d.ts
+++ types/Service.d.ts
@@ -1,3 +1,4 @@
+import axios, {AxiosResponse} from 'axios'
 export default class Service {
     constructor(Sws: any);
     private _sws;
@@ -37,3 +38,9 @@ export default class Service {
 }
 export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'OPTIONS';
 export type ResponseType = 'json' | 'blob';
+
+export class SwsError extends Error {
+    httpStatus: number
+    code?: number
+    response: AxiosResponse
+}
