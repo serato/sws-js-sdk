5b99f00f52ff6d86d11bc48e74f208837fc73780
HayleySharpeSerato <hayley.sharpe@serato.com>
2024-12-10 14:23:49 +1300

    remove unnecessary prototype setting, comment for clarity

--- src/SwsError.js
+++ src/SwsError.js
@@ -16,9 +16,9 @@ export default class SwsError extends Error {
    */
     constructor(message, httpStatus, response, code = undefined) {
         super(message)
+        // append additional properties to SwsError object for cleaner error handling in TS
         this.httpStatus = httpStatus
         this.response = response
         this.code = code
-        Object.setPrototypeOf(this, SwsError.prototype);
     }
 }
